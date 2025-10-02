6e24b81eb4542a60cca69e11fc1d770335f20407
Demon <skylerandrews003@gmail.com>
2025-10-02 13:41:52 -0700

    Updated selection functionality to send a POST call directly to a loupedeck HTTP server, allowing for remote access to the selection.json. Works locally, needs to be deployed and tested remotely

--- Source/VirtualBridge/Private/VirtualBridge.cpp
+++ Source/VirtualBridge/Private/VirtualBridge.cpp
@@ -4,8 +4,11 @@
 #include "Editor.h"
 #include "Engine/Selection.h"
 #include "GameFramework/Actor.h"
+#include "HttpModule.h"
+#include "Interfaces/IHttpRequest.h"
+#include "Interfaces/IHttpResponse.h"
 #include "Json.h"
-#include "HAL/PlatformFileManager.h"
+//#include "HAL/PlatformFileManager.h"
 #include "Misc/FileHelper.h"
 #include "Misc/Paths.h"
 
@@ -16,6 +19,8 @@ DEFINE_LOG_CATEGORY(LogVirtualBridge);
 void FVirtualBridgeModule::StartupModule()
 {
 	UE_LOG(LogVirtualBridge, Error, TEXT("=== VIRTUALBRIDGE PLUGIN LOADED SUCCESSFULLY! ==="));
+
+	LoadConfig();
 
 	if (GEditor)
 	{
@@ -35,9 +40,44 @@ void FVirtualBridgeModule::ShutdownModule()
 	if (SelectionChangedHandle.IsValid())
 	{
 		USelection::SelectionChangedEvent.Remove(SelectionChangedHandle);
-		UE_LOG(LogVirtualBridge, Warning, TEXT("Selection listener removed"));
 	}
 	UE_LOG(LogVirtualBridge, Error, TEXT("=== VIRTUALBRIDGE PLUGIN UNLOADED ==="));
+}
+
+void FVirtualBridgeModule::LoadConfig() {
+	// Load Loupedeck endpoint from config file
+	FString ConfigPath = FPaths::ProjectDir() + TEXT("VirtualBridgeConfig.json");
+	FString ConfigContent;
+
+	if (FFileHelper::LoadFileToString(ConfigContent, *ConfigPath)) {
+		TSharedPtr<FJsonObject> JsonObject;
+		TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(ConfigContent);
+
+		if (FJsonSerializer::Deserialize(Reader, JsonObject) && JsonObject.IsValid()) {
+			LoupedeckEndpoint = JsonObject->GetStringField("LoupedeckEndpoint");
+			UE_LOG(LogVirtualBridge, Log, TEXT("Loaded Loupedeck endpoint: %s"), *LoupedeckEndpoint);
+		}
+	}
+	else {
+		// Default fallback
+		LoupedeckEndpoint = TEXT("http://localhost:7070/selection");
+		UE_LOG(LogVirtualBridge, Warning, TEXT("Config not found, using default: %s"), *LoupedeckEndpoint);
+	}
+}
+
+void FVirtualBridgeModule::RegisterSelectionListener() {
+	if (GEditor && !SelectionChangedHandle.IsValid()) {
+		SelectionChangedHandle = USelection::SelectionChangedEvent.AddRaw(this, &FVirtualBridgeModule::OnSelectionChanged);
+		UE_LOG(LogVirtualBridge, Warning, TEXT("Selection listener registered"))
+	}
+}
+
+bool FVirtualBridgeModule::TryRegisterSelectionListener(float DeltaTime) {
+	if (GEditor) {
+		RegisterSelectionListener();
+		return false;
+	}
+	return true;
 }
 
 void FVirtualBridgeModule::OnSelectionChanged(UObject* Selection)
@@ -48,7 +88,7 @@ void FVirtualBridgeModule::OnSelectionChanged(UObject* Selection)
 	UE_LOG(LogVirtualBridge, Error, TEXT("GetSelectedActorPaths() returned %d items"), SelectedPaths.Num());
 
 	// Write selection to JSON file
-	WriteSelectionToFile(SelectedPaths);
+	// WriteSelectionToFile(SelectedPaths);
 
 	if (SelectedPaths.Num() > 0)
 	{
@@ -61,102 +101,149 @@ void FVirtualBridgeModule::OnSelectionChanged(UObject* Selection)
 	{
 		UE_LOG(LogVirtualBridge, Warning, TEXT("No actors selected"));
 	}
+
+	// Send to Loupedeck
+	SendSelectionToLoupedeck(SelectedPaths);
 }
 
 TArray<FString> FVirtualBridgeModule::GetSelectedActorPaths()
 {
-	UE_LOG(LogVirtualBridge, Error, TEXT("=== GetSelectedActorPaths() CALLED ==="));
-
 	TArray<FString> Paths;
 	if (GEditor)
 	{
-		int32 ActorCount = 0;
 		for (FSelectionIterator It(GEditor->GetSelectedActorIterator()); It; ++It)
 		{
-			ActorCount++;
 			if (AActor* Actor = Cast<AActor>(*It))
 			{
-				FString ActorPath = Actor->GetPathName();
-				Paths.Add(ActorPath);
-				UE_LOG(LogVirtualBridge, Error, TEXT("Found Actor #%d: %s"), ActorCount, *ActorPath);
+				Paths.Add(Actor->GetPathName());
 			}
 		}
-		UE_LOG(LogVirtualBridge, Error, TEXT("Total actors processed: %d"), ActorCount);
 	}
-	else
-	{
-		UE_LOG(LogVirtualBridge, Error, TEXT("GEditor is NULL in GetSelectedActorPaths()"));
-	}
-
 	return Paths;
 }
 
-void FVirtualBridgeModule::RegisterSelectionListener()
-{
-	if (GEditor && !SelectionChangedHandle.IsValid())
-	{
-		SelectionChangedHandle = USelection::SelectionChangedEvent.AddRaw(this, &FVirtualBridgeModule::OnSelectionChanged);
-		UE_LOG(LogVirtualBridge, Warning, TEXT("Selection change listener registered"));
-	}
-}
-
-bool FVirtualBridgeModule::TryRegisterSelectionListener(float DeltaTime)
-{
-	if (GEditor)
-	{
-		RegisterSelectionListener();
-		return false; // Stop the ticker
-	}
-
-	UE_LOG(LogVirtualBridge, Warning, TEXT("Still waiting for GEditor..."));
-	return true; // Keep trying
-}
-
-void FVirtualBridgeModule::WriteSelectionToFile(const TArray<FString>& Paths)
-{
-	// Create JSON object
+void FVirtualBridgeModule::SendSelectionToLoupedeck(const TArray<FString>& Paths) {
+	//create JSON payload
 	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
 
-	if (Paths.Num() > 0)
-	{
-		// Create JSON array for selected actors
+	if (Paths.Num() > 0) {
 		TArray<TSharedPtr<FJsonValue>> JsonArray;
-		for (const FString& Path : Paths)
-		{
+		for (const FString& Path : Paths) {
 			JsonArray.Add(MakeShareable(new FJsonValueString(Path)));
 		}
 
 		JsonObject->SetArrayField("selectedActors", JsonArray);
-		JsonObject->SetStringField("primarySelection", Paths[0]); // First selected actor
+		JsonObject->SetStringField("primarySelection", Paths[0]);
 		JsonObject->SetNumberField("count", Paths.Num());
 	}
-	else
-	{
-		// No selection
+	else {
 		JsonObject->SetArrayField("selectedActors", TArray<TSharedPtr<FJsonValue>>());
 		JsonObject->SetStringField("primarySelection", TEXT(""));
 		JsonObject->SetNumberField("count", 0);
 	}
 
-	JsonObject->SetStringField("timestamp", FDateTime::Now().ToString());
 	JsonObject->SetBoolField("hasSelection", Paths.Num() > 0);
 
-	// Convert to JSON string
+	// Serialize to string
 	FString OutputString;
 	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
 	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
 
-	// Write to file in project directory
-	FString FilePath = FPaths::ProjectDir() + TEXT("selection.json");
-	if (FFileHelper::SaveStringToFile(OutputString, *FilePath))
+	// Create HTTP Request
+	TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+	HttpRequest->SetVerb("POST");
+	HttpRequest->SetURL(LoupedeckEndpoint);
+	HttpRequest->SetHeader("Content-Type", "application/json");
+	HttpRequest->SetContentAsString(OutputString);
+	HttpRequest->OnProcessRequestComplete().BindRaw(this, &FVirtualBridgeModule::OnHttpResponseReceived);
+
+	if (HttpRequest->ProcessRequest()) {
+		UE_LOG(LogVirtualBridge, Log, TEXT("Sent selection to Loupedeck: %s"), *OutputString);
+	}
+	else {
+		UE_LOG(LogVirtualBridge, Error, TEXT("Failed to send HTTP request"));
+	}
+}
+
+void FVirtualBridgeModule::OnHttpResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful) {
+	if (bWasSuccessful && Response.IsValid())
 	{
-		UE_LOG(LogVirtualBridge, Log, TEXT("Selection written to: %s"), *FilePath);
+		UE_LOG(LogVirtualBridge, Log, TEXT("Loupedeck acknowledged: %d"), Response->GetResponseCode());
 	}
 	else
 	{
-		UE_LOG(LogVirtualBridge, Error, TEXT("Failed to write selection file"));
+		UE_LOG(LogVirtualBridge, Warning, TEXT("Failed to reach Loupedeck (is it running?)"));
 	}
 }
+
+
+// old
+//void FVirtualBridgeModule::RegisterSelectionListener()
+//{
+//	if (GEditor && !SelectionChangedHandle.IsValid())
+//	{
+//		SelectionChangedHandle = USelection::SelectionChangedEvent.AddRaw(this, &FVirtualBridgeModule::OnSelectionChanged);
+//		UE_LOG(LogVirtualBridge, Warning, TEXT("Selection change listener registered"));
+//	}
+//}
+//
+//bool FVirtualBridgeModule::TryRegisterSelectionListener(float DeltaTime)
+//{
+//	if (GEditor)
+//	{
+//		RegisterSelectionListener();
+//		return false; // Stop the ticker
+//	}
+//
+//	UE_LOG(LogVirtualBridge, Warning, TEXT("Still waiting for GEditor..."));
+//	return true; // Keep trying
+//}
+
+//void FVirtualBridgeModule::WriteSelectionToFile(const TArray<FString>& Paths)
+//{
+//	// Create JSON object
+//	TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject);
+//
+//	if (Paths.Num() > 0)
+//	{
+//		// Create JSON array for selected actors
+//		TArray<TSharedPtr<FJsonValue>> JsonArray;
+//		for (const FString& Path : Paths)
+//		{
+//			JsonArray.Add(MakeShareable(new FJsonValueString(Path)));
+//		}
+//
+//		JsonObject->SetArrayField("selectedActors", JsonArray);
+//		JsonObject->SetStringField("primarySelection", Paths[0]); // First selected actor
+//		JsonObject->SetNumberField("count", Paths.Num());
+//	}
+//	else
+//	{
+//		// No selection
+//		JsonObject->SetArrayField("selectedActors", TArray<TSharedPtr<FJsonValue>>());
+//		JsonObject->SetStringField("primarySelection", TEXT(""));
+//		JsonObject->SetNumberField("count", 0);
+//	}
+//
+//	JsonObject->SetStringField("timestamp", FDateTime::Now().ToString());
+//	JsonObject->SetBoolField("hasSelection", Paths.Num() > 0);
+//
+//	// Convert to JSON string
+//	FString OutputString;
+//	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&OutputString);
+//	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
+//
+//	// Write to file in project directory
+//	FString FilePath = FPaths::ProjectDir() + TEXT("selection.json");
+//	if (FFileHelper::SaveStringToFile(OutputString, *FilePath))
+//	{
+//		UE_LOG(LogVirtualBridge, Log, TEXT("Selection written to: %s"), *FilePath);
+//	}
+//	else
+//	{
+//		UE_LOG(LogVirtualBridge, Error, TEXT("Failed to write selection file"));
+//	}
+//}
 
 #undef LOCTEXT_NAMESPACE
 
--- Source/VirtualBridge/Public/VirtualBridge.h
+++ Source/VirtualBridge/Public/VirtualBridge.h
@@ -4,24 +4,27 @@
 
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
+#include "Http.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogVirtualBridge, Log, All);
 
 class FVirtualBridgeModule : public IModuleInterface
 {
 public:
-
-	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
 
 private:
     FDelegateHandle SelectionChangedHandle;
+    FString LoupedeckEndpoint;
 
     void OnSelectionChanged(UObject* Selection);
     TArray<FString> GetSelectedActorPaths();
     void RegisterSelectionListener();
     bool TryRegisterSelectionListener(float);
 
-    void WriteSelectionToFile(const TArray<FString>& Paths);
+    //void WriteSelectionToFile(const TArray<FString>& Paths);
+    void SendSelectionToLoupedeck(const TArray<FString>& Paths);
+    void OnHttpResponseReceived(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+    void LoadConfig();
 };
